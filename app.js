6f5879bfbb6f26a14cd260b5b0dffd7a2b2e7547
Michael Darmousseh <michael.darmousseh@gmail.com>
2012-11-27 13:59:37 -0800

    mustache layout file :D

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var express = require('express')
     , routes = require('./routes')
     , http = require('http')
     , path = require('path')
-    , cons = require('consolidate');
+    , cons = require('consolidate')
+    ,mustache = require("./lib/mustache");
 require("pg-simple").defaults.database_url = process.env.HEROKU_POSTGRESQL_CHARCOAL_URL || "postgres://postgres:michael@127.0.0.1:5432/galli_development";
+var redis, RedisStore;
+if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+}
 
 var app = express();
 
@@ -16,23 +23,28 @@ app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'html');
     app.set('view options', { layout:'application.html' });
-    app.engine(".mustache", cons.mustache);
-    app.engine(".html", cons.mustache);
+    app.engine(".mustache", mustache);
+    app.engine(".html", mustache);
 
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.cookieParser('Galli9000'));
-    app.use(express.cookieSession());
-    app.use(express.methodOverride());
-    app.use(app.router);
-
-    app.use(express.static(path.join(__dirname, 'public')));
 });
-
 app.configure('development', function () {
     app.use(express.errorHandler());
+    app.use(express.session({secret: 'lolcatsz'}));
 });
+app.configure('production', function() {
+    app.use(express.session({ store: new RedisStore({client: redis}), secret: 'lolcat' }));
+})
+
+app.configure(function() {
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+})
+
 
 routes.attach(app);
 
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 var _ = require('underscore')._
     , routes = require("express-http-routes")
     , pgSimple = require("pg-simple")
-    , User = require("../../models/user");
+    , User = require("../models/user");
 
 routes = {
     /*
      * GET home page.
      */
 
-    "GET /api/user":function (req, res) {
+    "GET /user":function (req, res) {
         if (req.session && req.session.user) {
             res.json(200, {login:req.session.user.login});
         } else {
@@ -17,7 +17,7 @@ routes = {
     },
 
 
-    "PUT /api/user":function (req, res) {
+    "PUT /user":function (req, res) {
         user = new User({email:req.body.email, password:req.body.password, password_confirmation:req.body.password_confirmation, login:req.body.login});
         req.session.user = user;
         res.json(200, req.session.user);
