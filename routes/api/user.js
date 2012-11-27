880165642152391483844230f40c08cd9c19b74f
Michael Darmousseh <michael.darmousseh@gmail.com>
2012-11-27 01:55:23 -0800

    oops

--- routes/api/user.js
+++ routes/api/user.js
@@ -9,21 +9,16 @@ routes = {
 
     "GET /user":function (req, res) {
         if (req.session && req.session.user) {
-            res.json(200, {login: req.session.user.login});
-        }  else {
+            res.json(200, {login:req.session.user.login});
+        } else {
             res.json(404, {});
         }
     },
 
 
-
-    "PUT /user": function(req, res) {
-        if (req.session && req.session.user) {
-            req.session.user = {email: req.body.email, password: req.body.password, password_confirmation: req.body.password_confirmation, login: req.body.login};
-            res.json(200, req.session.user);
-        }  else {
-            res.json(404, {});
-        }
+    "PUT /user":function (req, res) {
+        req.session.user = {email:req.body.email, password:req.body.password, password_confirmation:req.body.password_confirmation, login:req.body.login};
+        res.json(200, req.session.user);
     }
 }
 
